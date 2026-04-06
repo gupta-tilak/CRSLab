faa1fa0964a1e5c53c94c3c1ebe83c30e6a50d18
gupta-tilak <thetilakgupta@gmail.com>
2026-04-06 15:36:58 +0530

    fixing setup

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 from setuptools import setup, find_packages
 
-try:
-    import torch
-    import torch_geometric
-except Exception:
-    raise Exception('Please install PyTorch and PyTorch Geometric manually first.\n' +
-                    'View CRSLab GitHub page for more information: https://github.com/RUCAIBox/CRSLab')
-
 setup_requires = []
 
 install_requires = [
